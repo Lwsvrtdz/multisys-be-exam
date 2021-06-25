9c2f1e43534d506a92d45fa9d755e1cd1dea5b5b
Louise <louise.virtudazo@iserviceglobalbiz.com>
2021-06-25 15:35:40 +0800

    status code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,7 +51,7 @@ class UserController extends Controller
         return response()->json(
             [
             'access_token' => $token,
-            ]
+            ], 201
         );
     }
 
